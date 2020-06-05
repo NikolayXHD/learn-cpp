1ee548e639ad9b80dda8888ceb70fba29e839f36
NikolayXHD <hidalgo1984@list.ru>
2020-06-05 23:28:06 +0300

    template multiplication

--- app/main.cpp
+++ app/main.cpp
@@ -1,121 +1,69 @@
 #include <iostream>
 #include <map>
-#include <tuple>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
-// Перечислимый тип для статуса задачи
-enum class TaskStatus {
-  NEW,          // новая
-  IN_PROGRESS,  // в разработке
-  TESTING,      // на тестировании
-  DONE          // завершена
-};
+template<typename TKey, typename TValue>
+TValue Sqr(const TValue& original);
 
-// Объявляем тип-синоним для map<TaskStatus, int>,
-// позволяющего хранить количество задач каждого статуса
-using TasksInfo = map<TaskStatus, int>;
+template<typename TKey, typename TValue>
+map<TKey, TValue> Sqr(const map<TKey, TValue>& original);
 
-class TeamTasks {
- public:
-  // Получить статистику по статусам задач конкретного разработчика
-  [[nodiscard]] const TasksInfo &
-  GetPersonTasksInfo(const string &person) const {
-    const auto &it = infos_by_person_.find(person);
-    if (it == infos_by_person_.cend()) {
-      return empty_;
-    }
-    return it->second;
-  }
+template<typename TValue>
+vector<TValue> Sqr(const vector<TValue>& original);
 
-  // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-  void AddNewTask(const string &person) {
-    infos_by_person_[person][TaskStatus::NEW]++;
-  }
 
-  // Обновить статусы по данному количеству задач конкретного разработчика,
-  // подробности см. ниже
-  tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-      const string &person, int task_count) {
-    const auto &it_infos = infos_by_person_.find(person);
-    if (it_infos == infos_by_person_.cend()) {
-      return tie(empty_, empty_);
-    }
-    auto& infos = it_infos->second;
-    if (task_count == 0) {
-      return tie(empty_, infos);
-    }
 
-    const auto infos_copy = infos;
-    TasksInfo updated_infos;
-    TasksInfo intact_infos;
-    for (const auto& it_cp: infos_copy) {
-      auto status = it_cp.first;
-      if (status == TaskStatus::DONE) {
-        break;
-      }
-      int updated = min(task_count, it_cp.second);
-      int intact = it_cp.second - updated;
-      if (updated > 0) {
-        int& new_count = infos.find(status)->second;
-        new_count -= updated;
-        if (new_count == 0) {
-          infos.erase(status);
-        }
-        auto next_status = static_cast<TaskStatus>((static_cast<int>(status) + 1));
-        infos[next_status] += updated;
-        updated_infos[next_status] = updated;
-      }
-
-      if (intact > 0) {
-        intact_infos[status] = updated;
-      }
-    }
-
-    return tie(updated_infos, intact_infos);
-  }
-
- private:
-  map<string, TasksInfo> infos_by_person_{};
-  const TasksInfo empty_{};
-};
-
-// Принимаем словарь по значению, чтобы иметь возможность
-// обращаться к отсутствующим ключам с помощью [] и получать 0,
-// не меняя при этом исходный словарь
-void PrintTasksInfo(TasksInfo tasks_info) {
-  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
-       ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
-       ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
-       ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+template<typename TKey, typename TValue>
+pair<TKey, TValue> operator*(pair<TKey, TValue> p1, pair<TKey, TValue> p2) {
+  return pair<TKey, TValue>(
+      p1.first * p2.first, p1.second * p2.second
+  );
 }
 
-int main() {
-  TeamTasks tasks;
-  tasks.AddNewTask("Ilia");
-  for (int i = 0; i < 3; ++i) {
-    tasks.AddNewTask("Ivan");
+template<typename TValue>
+TValue Sqr(const TValue& original) {
+  return original * original;
+}
+
+template<typename TKey, typename TValue>
+map<TKey, TValue> Sqr(const map<TKey, TValue>& original) {
+  map<TKey, TValue> result;
+  for (const auto& [key, value]: original) {
+    result[key] = Sqr(value);
   }
-  cout << "Ilia's tasks: ";
-  PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-  cout << "Ivan's tasks: ";
-  PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+  return result;
+}
 
-  TasksInfo updated_tasks, untouched_tasks;
+template<typename TValue>
+vector<TValue> Sqr(const vector<TValue>& original) {
+  vector<TValue> result;
+  result.reserve(original.size());
+  for (const auto& value: original) {
+    result.push_back(Sqr(value));
+  }
+  return result;
+}
 
-  tie(updated_tasks, untouched_tasks) =
-      tasks.PerformPersonTasks("Ivan", 2);
-  cout << "Updated Ivan's tasks: ";
-  PrintTasksInfo(updated_tasks);
-  cout << "Untouched Ivan's tasks: ";
-  PrintTasksInfo(untouched_tasks);
 
-  tie(updated_tasks, untouched_tasks) =
-      tasks.PerformPersonTasks("Ivan", 2);
-  cout << "Updated Ivan's tasks: ";
-  PrintTasksInfo(updated_tasks);
-  cout << "Untouched Ivan's tasks: ";
-  PrintTasksInfo(untouched_tasks);
+int main() {
+  // Пример вызова функции
+  vector<int> v = {1, 2, 3};
+  cout << "vector:";
+  for (int x : Sqr(v)) {
+    cout << ' ' << x;
+  }
+  cout << endl;
 
+  map<int, pair<int, int>> map_of_pairs = {
+      {4, {2, 2}},
+      {7, {4, 3}}
+  };
+  cout << "map of pairs:" << endl;
+  for (const auto& x : Sqr(map_of_pairs)) {
+    cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+  }
   return 0;
 }
